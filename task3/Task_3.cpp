85852401a14d5abb5f1f465ddf8d1644dbb3a056
Unknown <e.gondyul@g.nsu.ru>
2022-01-19 10:30:06 +0700

    third ed

--- task3/Task_3.cpp
+++ task3/Task_3.cpp
@@ -7,26 +7,8 @@
 int main(int argc, char** argv) {
 
 
-	auto stream = makeStream({ 1,2 });
+	auto stream = makeStream({ 1,3 });
 	auto result = stream | sum();
-
-
-	//auto result = stream | map([](auto a) -> auto {return a; }) | to_vector();
-	//std::cout << "IMHERE " << result.size() << std::endl;
-	//auto& vec = result.at(0);
-//std::cout << vec.size() << std::endl;
-//for (int i = 0; i < 2; i++){
-//	std::cout << vec.at(i);
-//}
-
-//std::vector<A> v(4);
-//auto stream = makeStream(v);
-//stream | map([](auto a) {return a;}) | print_to(std::cout, " ");
-
-//for (auto i = result.begin(); i != result.end(); i++){
-//	std::cout << *i << std::endl;
-//}
-
 
 	return 0;
 }
